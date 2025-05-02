fa0677f8d9fe3449a876b3228b90dfdd9467cfe1
Sourav6803 <souravbhukta8@gmail.com>
2025-05-02 13:19:58 +0530

    cors add

--- app.ts
+++ app.ts
@@ -9,12 +9,34 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// app.use(cors({
+//     origin: ["https://chinta-acdemy-frontend-whhp.vercel.app","http://localhost:3000"],
+//     credentials: true
+// }));
+
+
 app.use(cors({
-    origin: ["https://chinta-acdemy-frontend-whhp.vercel.app","http://localhost:3000"],
-    credentials: true
-}));
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        'https://chinta-acdemy-frontend-whhp.vercel.app',
+        'http://localhost:3000',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
+  
+  // Add this middleware to handle preflight requests
+  app.options('*', cors());
+  
+
 app.use(express.json());
 app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminroutes);
